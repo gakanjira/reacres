d4cc948eb5ac12d8229198024f350fbc54de9266
Dawid Łakomiec <gakanjira@MacBook-Pro-Dawid.local>
2020-06-10 10:05:49 +0200

    add global styles using styled components

--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import Button from 'components/Button/Button';
+import GlobalStyle from 'theme/GlobalStyle';
 
-const App = () => <h1>Hello Gaka</h1>;
+const App = () => (
+  <div>
+    <GlobalStyle />
+    <Button>Hello</Button>
+    <Button secondary>Hello</Button>
+  </div>
+);
 
 export default App;
