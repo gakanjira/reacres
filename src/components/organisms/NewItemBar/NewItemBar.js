03d069020d97b59c2b6add9cc37495346769908c
Dawid Łakomiec <gakanjira@MacBook-Pro-Dawid.local>
2020-06-15 13:48:25 +0200

    create form to add new item

---
+++ src/components/organisms/NewItemBar/NewItemBar.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import Input from 'components/atoms/Input/Input';
+import Button from 'components/atoms/Button/Button';
+import Heading from 'components/atoms/Heading/Heading';
+import withContext from 'hoc/withContext';
+
+const StyledWrapper = styled.div`
+  border-left: 5px solid ${({ theme, activeColor }) => theme[activeColor]};
+  z-index: 999;
+  position: fixed;
+  display: flex;
+  padding: 100px 50px;
+  justify-content: space-evenly;
+  flex-direction: column;
+  right: 0;
+  top: 0;
+  height: 100vh;
+  width: 680px;
+  background-color: #fff;
+  box-shadow: -5px 0 15px rgba(0, 0, 0, 0.3);
+  transform: translate(${({ isVisible }) => (isVisible ? '0' : '100%')});
+  transition: transform 0.3s ease-in-out;
+`;
+
+const StyledTextArea = styled(Input)`
+  margin: 30px 0 100px;
+  border-radius: 20px;
+  height: 30vh;
+`;
+
+const NewItemBar = ({ pageContext, isVisible }) => (
+  <StyledWrapper activeColor={pageContext} isVisible={isVisible}>
+    <Heading big>Create new {pageContext} </Heading>
+    <Input placeholder="title" />
+    {pageContext === 'articles' ? <Input placeholder="link" /> : null}
+    <StyledTextArea as="textarea" placeholder="content" />
+    <Button>Add note</Button>
+  </StyledWrapper>
+);
+
+NewItemBar.propTypes = {
+  pageContext: PropTypes.oneOf(['notes', 'articles', 'twitters']),
+  isVisible: PropTypes.bool,
+};
+
+NewItemBar.defaultProps = {
+  pageContext: 'notes',
+  isVisible: false,
+};
+
+export default withContext(NewItemBar);
--- src/templates/GridTemplate.js
+++ src/templates/GridTemplate.js
@@ -6,8 +6,12 @@ import Input from 'components/atoms/Input/Input';
 import Heading from 'components/atoms/Heading/Heading';
 import Paragraph from 'components/atoms/Paragraph/Paragraph';
 import UserPageTemplate from 'templates/UserPageTemplate';
+import ButtonIcon from 'components/atoms/ButtonIcon/ButtonIcon';
+import NewItemBar from 'components/organisms/NewItemBar/NewItemBar';
+import plusIcon from 'assets/icons/plus.svg';
 
 const StyledWrapper = styled.div`
+  position: relative;
   padding: 25px 150px 25px 70px;
 `;
 
@@ -29,26 +33,61 @@ const StyledHeading = styled(Heading)`
   }
 `;
 
-const GridTemplate = ({ children, pageType }) => (
-  <UserPageTemplate pageType={pageType}>
-    <StyledWrapper>
-      <StyledPageHeader>
-        <Input search placeholder="search" />
-        <StyledHeading big>{pageType}s</StyledHeading>
-        <Paragraph># {pageType}s</Paragraph>
-      </StyledPageHeader>
-      <GridWrapper>{children}</GridWrapper>
-    </StyledWrapper>
-  </UserPageTemplate>
-);
+const StyledButtonIcon = styled(ButtonIcon)`
+  position: fixed;
+  bottom: 40px;
+  right: 40px;
+  background-color: ${({ activeColor, theme }) => theme[activeColor]};
+  border-radius: 50%;
+  z-index: 999999999;
+`;
+
+class GridTemplate extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isNewItemBarVisible: false,
+    };
+  }
+
+  handleNewItemBarToggle = () => {
+    this.setState((prevState) => ({
+      isNewItemBarVisible: !prevState.isNewItemBarVisible,
+    }));
+  };
+
+  render() {
+    const { pageContext, children } = this.props;
+    const { isNewItemBarVisible } = this.state;
+    return (
+      <UserPageTemplate pageContext={pageContext}>
+        <StyledWrapper>
+          <StyledPageHeader>
+            <Input search placeholder="search" />
+            <StyledHeading big>{pageContext}s</StyledHeading>
+            <Paragraph># {pageContext}s</Paragraph>
+          </StyledPageHeader>
+          <GridWrapper>{children}</GridWrapper>
+          <NewItemBar isVisible={isNewItemBarVisible} />
+          <StyledButtonIcon
+            activeColor={pageContext}
+            icon={plusIcon}
+            onClick={this.handleNewItemBarToggle}
+          />
+        </StyledWrapper>
+      </UserPageTemplate>
+    );
+  }
+}
 
 GridTemplate.propTypes = {
   children: PropTypes.arrayOf(PropTypes.Object).isRequired,
-  pageType: PropTypes.oneOf(['notes', 'articles', 'twitters']),
+  pageContext: PropTypes.oneOf(['notes', 'articles', 'twitters']),
 };
 
 GridTemplate.defaultProps = {
-  pageType: 'notes',
+  pageContext: 'notes',
 };
 
 export default withContext(GridTemplate);
